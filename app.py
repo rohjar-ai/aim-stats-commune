fbade2189d743e5c0d11eb8aa14f3512a93a43d7
root <root@vmi1838796.contaboserver.net>
2024-05-07 19:40:33 +0200

    fix: use real miners data

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='.')
 
+from communex.misc import get_map_modules
+from communex.cli._common import (make_custom_context, ExtraCtxData)
+from rich.console import Console
+import typer
+from typer import Context
+from communex.types import ModuleInfoWithOptionalBalance
+
 
 @app.route('/')
 def home():
@@ -12,8 +19,21 @@ def home():
 
 def get_data():
     # Your code to fetch data
-    data = ["ONE", "TWO", "THREE"]
-    return data
+    
+    ctx = typer.Context
+    ctx.obj = ExtraCtxData(output_json=False, use_testnet=False)
+
+
+    context = make_custom_context(ctx)
+    client = context.com_client()
+    modules = get_map_modules(client, netuid=17, include_balances=False)
+
+    data = []
+    for module_info in modules.values():
+        data.append(module_info['name'])
+        print(f"NAME: {module_info['name']}, EMISSION: {module_info['emission']}")
+    
+    return modules.values()
 
 if __name__ == '__main__':
     app.run(debug=True)
