4cdf4650f11530b7e4790ecad5c91f1f4faf600a
root <root@vmi1838796.contaboserver.net>
2024-05-07 16:00:22 +0200

    chore: change data source

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     # Fetch your data
     data = get_data()
-    
-    print(data)
     # Pass data to the template
     return render_template('index.html', data=data)
 
