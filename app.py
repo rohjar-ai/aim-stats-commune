de769406b8e2739f7f46c4c50dfb4bdf32a78af1
root <root@vmi1838796.contaboserver.net>
2024-05-07 13:56:48 +0200

    chore: display a list

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 def home():
     # Fetch your data
     data = get_data()
+    
+    print(data)
     # Pass data to the template
     return render_template('index.html', data=data)
 
 def get_data():
     # Your code to fetch data
-    data = list("ONE", "TWO", "THREE")
+    data = ["ONE", "TWO", "THREE"]
     return data
 
 if __name__ == '__main__':
