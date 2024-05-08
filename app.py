d79c91879023a309eade0b9c0142e8883c80ca78
root <root@vmi1838796.contaboserver.net>
2024-05-08 16:57:04 +0200

    chore: make app public

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+import typer
+from communex.misc import get_map_modules
+from communex.cli._common import make_custom_context, ExtraCtxData
+from communex.types import ModuleInfoWithOptionalBalance
+from flask_cors import CORS
+import json
 
 app = Flask(__name__, template_folder='.')
-
-from communex.misc import get_map_modules
-from communex.cli._common import (make_custom_context, ExtraCtxData)
-from rich.console import Console
-import typer
-from typer import Context
-from communex.types import ModuleInfoWithOptionalBalance
-
+CORS(app)
 
 @app.route('/')
 def home():
-    # Fetch your data
     data = get_data()
-    # Pass data to the template
-    return render_template('index.html', data=data)
+    return jsonify(data)
 
 def get_data():
-    # Your code to fetch data
-    
     ctx = typer.Context
     ctx.obj = ExtraCtxData(output_json=False, use_testnet=False)
-
-
     context = make_custom_context(ctx)
     client = context.com_client()
     modules = get_map_modules(client, netuid=17, include_balances=False)
-
-    data = []
-    for module_info in modules.values():
-        data.append(module_info['name'])
-        print(f"NAME: {module_info['name']}, EMISSION: {module_info['emission']}")
     
-    return modules.values()
+    return list(modules.values())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
