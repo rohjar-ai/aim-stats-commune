d46e5a63d5dd63d02c2a42986b05bd7796f869e2
rohjar <rohjar.ai@gmail.com>
2024-06-10 21:20:21 +0200

    chore: move stats collection to separate unit

--- aim_stats_commune/app.py
+++ aim_stats_commune/app.py
@@ -1,66 +1,21 @@
-import threading
-import typer
-
-from apscheduler.schedulers.background import BackgroundScheduler
-from communex.cli._common import make_custom_context, ExtraCtxData
-from communex.misc import get_map_modules
-from datetime import datetime
-from aim_stats_commune.discord_delegate import send_message
+from commune_stats_collector import CommuneStatsCollector
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='.')
 CORS(app)
-
-scheduler = BackgroundScheduler()
-data_lock = threading.Lock()
-shared_data = None
-
-
-def collect_subnet_data():
-    ctx = typer.Context
-    ctx.obj = ExtraCtxData(output_json=False, use_testnet=False, yes_to_all=False)
-    context = make_custom_context(ctx)
-    client = context.com_client()
-
-    modules = get_map_modules(client, netuid=17, include_balances=False)
-    if not modules:
-        send_message("Error getting modules using comx! Empty list returned. Retrying...")
-        modules = get_map_modules(client, netuid=17, include_balances=False)
-        if not modules:
-            send_message("Error getting modules using comx! Empty list returned again.")
-    return list(modules.values())
-
-
-def collect_in_background():
-    global shared_data
-    print(f"Collecting data: {datetime.now()}")
-    collected_data = collect_subnet_data()
-    with data_lock:
-        shared_data = collected_data
-    print("Collecting completed.")
-
-
-with app.app_context():
-    scheduler.add_job(collect_in_background, 'date')
-    scheduler.start()
+stats_collector = CommuneStatsCollector()
 
 
 @app.route('/')
 def home():
-    with data_lock:
-        subnet_data = shared_data if shared_data else "Collecting data..."
+    subnet_data = stats_collector.get()
     return jsonify(subnet_data)
-
-
-@scheduler.scheduled_job('interval', minutes=5)
-def scheduled_job():
-    collect_in_background()
 
 
 @app.cli.command()
 def shutdown():
-    scheduler.shutdown(wait=False)
+    stats_collector.stop()
 
 
 def main():
